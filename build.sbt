7fe8110cef8e9573f300ba73125aa71a615c1803
chenyu <luchenyuxx@gmail.com>
2017-05-30 17:29:36 +0200

    a simple web crawler implementation using akka

--- build.sbt
+++ build.sbt
@@ -12,5 +12,9 @@ lazy val root = (project in file(".")).
     libraryDependencies ++= Seq(
       "com.typesafe.akka" %% "akka-actor" % "2.5.2",
       "com.typesafe.akka" %% "akka-testkit" % "2.5.2" % Test
-    )
+    ),
+    // Used for HTML parse
+    libraryDependencies += "org.jsoup" % "jsoup" % "1.10.2",
+    // Used for URL validate
+    libraryDependencies += "commons-validator" % "commons-validator" % "1.6"
   )
---
+++ src/main/scala/cookietracker/Indexer.scala
@@ -0,0 +1,23 @@
+package cookietracker
+
+import java.net.URL
+
+import akka.actor.{Actor, ActorRef}
+
+class Indexer(supervisor: ActorRef) extends Actor {
+  var store = Map.empty[URL, Content]
+
+  def receive: Receive = {
+    case Index(url, content) =>
+      println(s"saving page $url with $content")
+      store += (url -> content)
+      supervisor ! IndexFinished(url, content.urls)
+  }
+
+  @throws[Exception](classOf[Exception])
+  override def postStop(): Unit = {
+    super.postStop()
+    store.foreach(println)
+    println(store.size)
+  }
+}
