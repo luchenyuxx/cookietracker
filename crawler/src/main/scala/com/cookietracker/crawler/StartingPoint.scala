6f732970cbb96f3bc44171785654aaf74b0ee174
chenyu <luchenyuxx@gmail.com>
2017-07-12 13:56:03 +0200

    remove dns resolver

--- crawler/src/main/scala/com/cookietracker/crawler/StartingPoint.scala
+++ crawler/src/main/scala/com/cookietracker/crawler/StartingPoint.scala
@@ -8,10 +8,10 @@ import scala.io.StdIn
 import scala.language.postfixOps
 
 object StartingPoint extends App {
-  val system = ActorSystem()
+  val system = ActorSystem("cookietracker")
   val webCrawler = system.actorOf(WebCrawler.props, "web-crawler")
 
-  webCrawler ! DequeueResult(new URL("http://en.wikipedia.org/wiki/Main_Page"))
+  webCrawler ! DequeueResult(new URL("https://en.wikipedia.org/wiki/Main_Page"))
   println("Web crawler started, press RETURN to exit.")
   StdIn.readLine()
 
--- crawler/src/main/scala/com/cookietracker/crawler/WebCrawler.scala
+++ crawler/src/main/scala/com/cookietracker/crawler/WebCrawler.scala
@@ -14,27 +14,23 @@ object WebCrawler {
   def props = Props(new WebCrawler)
 }
 
-class WebCrawler extends Actor with ActorLogging{
+class WebCrawler extends Actor with ActorLogging {
   implicit val contextExecutor = context.dispatcher
 
   lazy val httpFetchers: ActorRef = context.actorOf(BalancingPool(5).props(HttpFetcher.props), "http-fetcher-router")
-  lazy val dnsResolver: ActorRef = context.actorOf(DnsResolver.props, "dns-resolver")
+//  lazy val dnsResolver: ActorRef = context.actorOf(DnsResolver.props, "dns-resolver")
   lazy val linkExtractors: ActorRef = context.actorOf(BalancingPool(5).props(LinkExtractor.props), "link-extractor-router")
   lazy val urlFrontier: ActorRef = context.actorOf(UrlFrontier.props, "url-frontier")
   lazy val urlFilter: ActorRef = context.actorOf(UrlFilter.props, "url-filter")
   lazy val urlDeduplicator: ActorRef = context.actorOf(UrlDeduplicator.props, "url-deduplicator")
 
   override def receive: Receive = {
-    // When receive a URL from UrlFrontier, we send it to DnsResolver
+    /** When receive a URL from UrlFrontier, we send it to DnsResolver
+      * Here we don't pass the URL to dns resolver, because Akka HTTP will deal with
+      * the DNS cache for us.
+      */
     case DequeueResult(url) =>
-      dnsResolver ! DnsResolve(url)
-    // After DNS is resolved, we fetch the URL
-    case DnsResolved(url, address) =>
-      val newUrl = address match {
-        case Some(ip) => new URL(url.getProtocol, ip.getHostAddress, url.getPort, url.getFile)
-        case None => url
-      }
-      httpFetchers ! Fetch(url, HttpRequest(uri = Uri(newUrl)))
+      httpFetchers ! Fetch(url, HttpRequest(uri = Uri(url)))
     // Having the fetch result, we extract links from it and get a new URL to fetch
     case FetchResult(url, responseFuture) =>
       linkExtractors ! ExtractLink(url, responseFuture.map(_.entity))
