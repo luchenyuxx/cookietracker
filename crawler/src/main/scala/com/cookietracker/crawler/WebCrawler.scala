9f7e9208702b6dda025e7d10fc02583d18ba31e1
chenyu <luchenyuxx@gmail.com>
2017-07-24 15:37:57 +0200

    add agent header to request.

--- crawler/src/main/scala/com/cookietracker/crawler/WebCrawler.scala
+++ crawler/src/main/scala/com/cookietracker/crawler/WebCrawler.scala
@@ -1,7 +1,7 @@
 package com.cookietracker.crawler
 
 import akka.actor.{Actor, ActorLogging, ActorRef, Props}
-import akka.http.scaladsl.model.{HttpRequest, Uri}
+import akka.http.scaladsl.model.{HttpHeader, HttpRequest, Uri, headers}
 import akka.pattern._
 import akka.routing.BalancingPool
 import akka.util.Timeout
@@ -37,7 +37,11 @@ class WebCrawler extends Actor with ActorLogging {
       implicit val timeout = Timeout(3.seconds)
       urlFrontier ? Dequeue onSuccess {
         case DequeueResult(url) =>
-          Try(Fetch(url, HttpRequest(uri = Uri(url.toExternalForm)))) match {
+          Try {
+            val userAgentHeader: HttpHeader = headers.`User-Agent`("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36")
+            val request = HttpRequest(uri = Uri(url.toExternalForm), headers = List(userAgentHeader))
+            Fetch(url, request)
+          } match {
             case Success(v) => fetcher ! v
             case Failure(t) =>
               log.error("error when creating HTTP request", t)
