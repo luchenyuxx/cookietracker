2181ae2d62de5fdc5a9c7eb9c94bb04550ed4cdf
chenyu <luchenyuxx@gmail.com>
2017-07-29 22:12:42 +0200

    Work on CookieManager

--- common/src/main/scala/com/cookietracker/common/data/Models.scala
+++ common/src/main/scala/com/cookietracker/common/data/Models.scala
@@ -1,5 +1,6 @@
 package com.cookietracker.common.data
 
+import java.net.URL
 import java.util.Date
 
 import org.mongodb.scala.bson.ObjectId
@@ -8,12 +9,20 @@ object WebHost {
   def apply(hostName: String, domain: Domain): WebHost = WebHost(new ObjectId, hostName, domain)
 }
 
+final case class WebHost(_id: ObjectId, hostName: String, domain: Domain)
+
+object Domain {
+  def apply(name: String): Domain = new Domain(new ObjectId, name)
+}
+
 final case class Domain(_id: ObjectId, name: String)
 
-final case class WebHost(_id: ObjectId, hostName: String, domain: Domain)
 
 final case class HostRelation(from: WebHost, to: WebHost, requestUrl: String)
 
+object Url {
+  def apply(protocol: String, host: WebHost, port: Int, file: String): Url = new Url(new ObjectId, protocol, host, port, file)
+}
 final case class Url(_id: ObjectId, protocol: String, host: WebHost, port: Int, file: String)
 
 final case class HttpCookie(_id: ObjectId,
--- crawler/src/main/scala/com/cookietracker/crawler/CookieManager.scala
+++ crawler/src/main/scala/com/cookietracker/crawler/CookieManager.scala
@@ -1,6 +1,7 @@
 package com.cookietracker.crawler
 
 import akka.actor.{Actor, ActorLogging, Props}
+import com.cookietracker.common.data.HttpCookie
 
 object CookieManager {
   def props = Props(new CookieManager)
@@ -9,6 +10,7 @@ object CookieManager {
 class CookieManager extends Actor with ActorLogging {
   override def receive: Receive = {
     case RecordCookie(url, cookies) =>
+    //      cookies.map(c => new HttpCookie(c.name, c.value, ))
     case GetCookie(url) => GetCookieResult(url, Seq())
   }
 }
