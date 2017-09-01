4062488a017f16359ffceab1bf390f214d445c39
chenyu <luchenyuxx@gmail.com>
2017-09-01 17:50:56 +0200

    When start, we won't enqueue an url to start. instead we load url to process from database

--- crawler/src/main/scala/com/cookietracker/crawler/StartingPoint.scala
+++ crawler/src/main/scala/com/cookietracker/crawler/StartingPoint.scala
@@ -5,7 +5,6 @@ import java.net.URL
 import akka.actor.{ActorSystem, PoisonPill}
 import com.cookietracker.common.data.SchemaChecker
 import com.cookietracker.common.database.PostgreSqlComponent
-import com.cookietracker.crawler.UrlDeduplicator.DeduplicateResult
 
 import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, ExecutionContext}
@@ -13,18 +12,21 @@ import scala.io.StdIn
 import scala.language.postfixOps
 
 object StartingPoint extends App {
+  def initialize(): Unit = {
+    checkDataBaseSchema()
+  }
   def checkDataBaseSchema(): Unit = {
     val checker = new SchemaChecker with PostgreSqlComponent
     checker.checkAndCreateTables(ExecutionContext.global)
   }
 
-  checkDataBaseSchema()
+  initialize()
 
   val system = ActorSystem("cookietracker")
   val webCrawler = system.actorOf(WebCrawler.props, "web-crawler")
   val startUrl = new URL("http://www.leparisien.fr/")
 
-  webCrawler ! DeduplicateResult(startUrl, Seq(startUrl))
+  //  webCrawler ! DeduplicateResult(startUrl, Seq(startUrl))
   webCrawler ! Start
 
   println("Web crawler started, press RETURN to exit.")
